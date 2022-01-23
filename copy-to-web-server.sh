677cb107643e85f0ad5b47444d664bf16f1c8ab3
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-01-23 20:41:31 +0000

    combine build script, it works!

--- copy-to-web-server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp index.html /var/www/vhosts/rahilpatel.com/httpdocs/test.html
-cp app.js.map /var/www/vhosts/rahilpatel.com/httpdocs/
-cp app.js /var/www/vhosts/rahilpatel.com/httpdocs/
