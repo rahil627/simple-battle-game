c655d51da104ea0b05daee8d2994f628e753c188
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-13 04:41:53 +0700

    Create Glob.hx

---
+++ src/Glob.hx
@@ -0,0 +1,38 @@
+// just an optional bare-bones ds to copy and begin a new project with
+// delete these lines and you're good to go!
+package hp.base; // DELETE ME!!
+import hp.base.*; // DELETE ME!!
+	
+import h2d.Layers;
+import h2d.Scene;  //import h3d.scene.Scene;
+
+// a static global helper class, with a scope specific to this app
+// store references to commonly-used data structures here
+// try to store only references of stuff, no actual objects (except simple types)
+// TODO: singleton vs static
+class AppGlobal {
+	
+	// only keep references to stuff here
+	public static var scene:Scene;
+	
+ 	public static var world:Layers;
+	
+	public static var entities:EntityList<Entity>;
+	//public static var players:Array<Player>;	
+	//public static var players:Vector<Player>; // or this, if the max amount of players never change
+	//public static var entitiesBackpack:Map<Int, TargetEntities>; // to search commonly-searched stuff by id (key)	
+
+
+	// constants
+	//public static inline final laserWidth = 50;
+
+}
+
+// careful: not to be confused with Layers with an 's'
+enum Layer {
+	background;
+	players;
+	projectiles;
+	effects;
+	ui;
+}
