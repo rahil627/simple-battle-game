fe652fadc65432b226f01c5da65fa33c4ed60961
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-13 06:57:30 +0700

    Update import.hx

--- src/import.hx
+++ src/import.hx
@@ -1,8 +1,6 @@
 // default imports applied to all in the path, recursively?
-import ra.*; // includes ra.Heaps, ra.Haxe, ra.hp.*
+//import ra.*; // includes ra.Heaps, ra.Haxe, ra.hp.*
 import ra.hp.*; // doesn't work recursively...
-// it might be better to make hp a separate package
-// but, it would suck to have 3 repos.. hrmm...
 
 import AppGlobal as GG;
 
