c28310f72a8c5fd1e5aa9efa7fa83f0cf5a026d2
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-13 05:13:06 +0700

    Update EntityList.hx

--- src/EntityList.hx
+++ src/EntityList.hx
@@ -1,8 +1,3 @@
-// just an optional bare-bones ds to copy and begin a new project with
-// delete these lines and you're good to go!
-package hp.base; // DELETE ME!!
-import hp.base.*; // DELETE ME!!
-
 import haxe.ds.List;
 
 // a specialized generic container for Entity
