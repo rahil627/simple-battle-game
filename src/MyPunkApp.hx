3132445bf966cc35867a7ea31584f74a05a2ed62
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-13 05:23:16 +0700

    Update MyPunkApp.hx

--- src/MyPunkApp.hx
+++ src/MyPunkApp.hx
@@ -10,6 +10,10 @@ class MyPunkApp extends PunkApp {
 	var world:Layers; // just a helper tool to put things in the right place in a 2d scene tree
 	var entities:EntityList<Entity>;
 	//var players:Array<PlayerEntity>; // TODO: static vs global reference
+	
+	var player:Player;
+	var justTouchPressed = false;
+
 
 	// called during application setup after `loadAssets` completed (from docs)
 	override function init() {
@@ -43,6 +47,12 @@ class MyPunkApp extends PunkApp {
 		 entities = new EntityList<Entity>();
 		 //players = new Array<PlayerEntity>();
 		 //var monster = new Entity(); // just be sure to store the reference to entities somewhere safe!
+		
+		player = new Player(HP.scene, 0x0000FF);
+
+		// TODO temp solution until i fix keys
+		HP.screenInputHandler.onPush = touchPressed;
+
 
 	}
 	
@@ -57,8 +67,59 @@ class MyPunkApp extends PunkApp {
 		super.update(dt);
 		entities.update(dt);
 		
+		// ideally:
+		// do logic stuff based on input *first*
+		// *then* draw
+		
+		// update game stuff	
+		player.update();
+		
+		// TODO: testing input continued
+		if (Key.isPressed(Key.K) || Key.isDown(Key.J) || Key.isDown('H'.code)) {
+			HP.console.log("yay, the buttons finally work!!");
+
+		}
+
+		if (justTouchPressed) {
+			justTouchPressed = false;
+			
+			new Laser(HP.scene, Math.random() * HP.scene.width, Math.random() * HP.scene.height, 0, Haxe.randomHex()); // Math.random() * Math.PI * 2
+			new Bomb(HP.scene, Math.random() * HP.sceneWidth, Math.random() * HP.sceneHeight, 0, 0xFF0000, Haxe.randomHex(), .7);
+		}
+
+	
+
+
+
+		// check collision between player and laser
+		// Objects have an auto-magic getBounds function tho,
+		// see interactive2d sample, looks simple!! :)
+
+		// check collisions
+		// if (player.getBounds.intersects(laserColliders)) {
+		// do something
+		// }
+
+		// laser needs to be it's own object
+		// objects have alpha, i think, which can be used
+		// once it's time is up, can remove it
+
+		// you can, however, reserve some memory for a few... 100?
+		// that way, there's no need to allocate and de-allocate memory
+		// for every laser
+
+		// just use Tile.fromColor(0xFFFFFF) to create the tile
+		// then pass it into the bitmap constructor
+
+		// the Graphics class might just be drawing diredctly to the screen!..
+		
 		
 	}
+	
+	function touchPressed(e:hxd.Event) {
+		justTouchPressed = true;
+	}
+
 	
 	// note: must be placed at the end? threw an error:
 	// module main didn't define object type Main?
