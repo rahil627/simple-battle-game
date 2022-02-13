65d3dd584330e8855c10375ec61540a903c42ad5
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-13 07:02:12 +0700

    Update import.hx

--- src/import.hx
+++ src/import.hx
@@ -1,5 +1,4 @@
-// default imports applied to all in the path, recursively?
-//import ra.*; // includes ra.Heaps, ra.Haxe
+//import ra.*; // my helper classes: ra.Heaps, ra.Haxe
 import ra.hp.HP; // HeapsPunk globals
 import AppGlobal as GG; // game globals
 
