0d9a5473533f27e136a058c711ad4f36f4f3ac94
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-15 16:32:30 +0700

    Update Bomb.hx

--- src/Bomb.hx
+++ src/Bomb.hx
@@ -5,49 +5,56 @@ import h2d.Object;
 class Bomb extends Entity {
 	
 	var sprite:Graphics;
+	var explosion:Graphics;
 
-	public override function new(parent:Object, x:Float = 100, y:Float = 100, r:Float = 0, bombColor:Int = 0xFF0000, laserColor:Int = 0xFFFFFF, alpha:Float = 1) {
-		super(parent);
+	public override function new(x:Float = 100, y:Float = 100, r:Float = 0, bombColor:Int = 0xFF0000, laserColor:Int = 0xFFFFFF, alpha:Float = 1) {
+		super();
+		
+		// set Objects' transforms
 		sprite = new Graphics(); // don't use constructor parameter parent, use scene.add for Layers
-		//this.x = 0; // don't use these! just draw using absolute coordinates
-		//this.y = 0;
-		sprite.rotation = r;
+		this.x = x;
+		this.y = y;
+		//sprite.rotation = r; // rotate after drawing? // TODO: test this
+		
+		explosion = new Graphics();
+		explosion.x = x;
+		explosion.y = y;
+		//explosion.rotation = r; // rotate after drawing
 
-
-		// init
+		// set stuff
 		var w = GG.laserWidth;
 
 		// draw bomb
+		// could just use a bitmap...
 		sprite.beginFill(bombColor, alpha);
 		sprite.drawRect(x, y, w, w);
 		sprite.endFill();
-		sprite.tile.center(); // TODO: does this work?
+		sprite.tile.center(); // TODO: does this work?, it doesn't have a tile... does it?
+		sprite.rotation = r;
+		
 		// draw laser-like explosion
+		// draw horizontal line
+		explosion.lineStyle(w, laserColor, alpha);
+		explosion.moveTo(0, y);
+		explosion.lineTo(HP.scene.width, y); // draws off-screen, but don't think it matters...
+	
+		// draw vertical line
+		explosion.lineStyle(w, laserColor, alpha);
+		explosion.moveTo(x, 0);
+		explosion.lineTo(x, HP.scene.height);
+		
+		explosion.rotation = r;
 
+		GG.scene.add(sprite, GG.Layer.projectiles.getIndex());
+		GG.entities.add(this);
+		
 		/*
 		// TODO: test local 'n global positions
 		var p = this.localToGlobal();
 		this.beginFill(0x00FF00, .8);
-		this.drawRect(0, 0, w, w);
+		this.drawRect(p.x, p.y, w, w);
 		this.endFill();
-
-		// then just draw from p0,0 to 4 directions
-		// TODO: local coordinates is actually much nicer,
-		// keeping the object all-together in one place
 		*/
-
-		// draw horizontal line (without any rotation)
-		sprite.lineStyle(w, laserColor, alpha);
-		sprite.moveTo(0, y);
-		sprite.lineTo(HP.scene.width, y);
-	
-		// draw vertical line
-		sprite.lineStyle(w, laserColor, alpha);
-		sprite.moveTo(x, 0);
-		sprite.lineTo(x, HP.scene.height);
-
-		GG.scene.add(sprite, GG.Layer.projectiles.getIndex());
-		GG.entities.add(this);
 	}
 
 
