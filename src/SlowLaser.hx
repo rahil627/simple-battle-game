9e4d2c69fdc58d33fcd7bd7b4bb1a00131d13625
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-15 17:40:18 +0700

    Update SlowLaser.hx

--- src/SlowLaser.hx
+++ src/SlowLaser.hx
@@ -46,4 +46,26 @@ class extends SlowLaser {
 		super.update(dt);
 
 	}
+
+	// careful: Object already has function named draw
+	// TODO: maybe should keep all draw functions seperate from logic
+	// that way, can just call all the draw functions at once
+	public function drawLaser(x:Float, y:Float, x2:Float, y2:Float, width:Int = 10, ?color:Int = 0xFFFFF, alpha:Float = 1.0) {
+		//sprite.clear()?
+		sprite.lineStyle(width, color, alpha); 
+		//sprite.drawLine(this.x, this.y, x2, y2);
+		sprite.moveTo(x, y);
+		sprite.lineTo(x2, y2);
+	}
+
+	// TODO: surely there are some simple stuff like sprite somewhere..
+	// note: structs are useful for returning multiple data fields,
+	// but should avoid passing them in, to keep the function more flexible 'n simple
+	private function getEndPoint(startingX:Float, startingY:Float, angle:Float, distance:Float):Point {	
+		var x2 = startingX + distance * Math.cos(angle);
+		var y2 = startingY + distance * Math.sin(angle);
+		return new Point(x2, y2);
+	}	
+	
+	
 }
