1ee67beeec51757ad346b57167c0073cc4fd0571
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-04-16 00:46:25 +0000

    rename to build.hxml for LSP

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-haxe compiler-config.hxml
+haxe build.hxml
 cp -f index.html /var/www/vhosts/rahilpatel.com/httpdocs/test.html
 cp -f app.js.map /var/www/vhosts/rahilpatel.com/httpdocs/
 cp -f app.js /var/www/vhosts/rahilpatel.com/httpdocs/
